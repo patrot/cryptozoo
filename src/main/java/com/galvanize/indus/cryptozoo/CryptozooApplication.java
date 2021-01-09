896d0f3542d595303725b57dd8630b1cb2b0d2b1
Trotter <856439@cognizant.com>
2021-01-09 13:35:35 -0600

    Create new Spring Web Project for Crypto Zoo

---
+++ src/main/java/com/galvanize/indus/cryptozoo/CryptozooApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.indus.cryptozoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptozooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptozooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/indus/cryptozoo/CryptozooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.indus.cryptozoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptozooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
